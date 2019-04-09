d50b7a6f03318e271834cfa6c33a7f564926a56b
Cinnam1 <theodore20068@gmail.com>
2019-04-09 08:55:49 +0200

    Har addet comments
    har addet comments så det bliver overskueligt

--- SO2 prototype 1/global.h
+++ SO2 prototype 1/global.h
@@ -3,13 +3,16 @@
 #include <string.h>
 #define NUMBEROFMEDIUMWORDS 8
 #define NUMBEROFSMALLWORDS 5
+///Nøgleord
 char *bigKeyword[];
 
 char *mediumKeyword[] = {"FEMINIST", "ASSAULT", "MAN", "ANTI", "DEMOCRAT", "RALLY", "INFORMATION", "CLINTON"};
 
 char *smallKeyword[] = {"POPE","FLORIDA", "FAIL", "ULTIMATE"};
 
+char *antiWord[] = {"KOREA", "CHINA", "BARACK", "G20", "JERUSALEM", "SAD", "COURT"}; ///Ord der opstår oftere i rigtige artikler
+                                                                                    /// end i falske, giver -1 point.
 
-char *antiWord[] = {"KOREA", "CHINA", "BARACK", "G20", "JERUSALEM", "SAD", "COURT"};
 
+/// Point der skal sige brugeren, hvor stor sandsynligheden er for at det er fake news
 int fakeNewsPoints=0;
--- SO2 prototype 1/main.c
+++ SO2 prototype 1/main.c
@@ -3,18 +3,22 @@
 
 int main()
 {
-	char str[] = "TRUMP SUPPORTER ATTACKED BY HILLARY ZOMBIES";
+	char str[] = "TRUMP SUPPORTER ATTACKED BY HILLARY ZOMBIES"; ///String to analyze
+
+	///SPLIT STRING VARIABLES
 	int init_size = strlen(str);
 	char delim[] = " ";
-    char *word4Word[20];
+    char *word4Word[20]; ///Artikeltitlen, men opdelt i ord
 	int i=0;
 	char *ptr = strtok(str, delim);
-    int numberOfWords=0;
+    int numberOfWords=0; ///Bliver også brugt som antal ord i strengen der skal analyseres
+
+    ///LOOK AT txtFile (and split the string)
     FILE * fPointer;
     fPointer = fopen("bigWords.txt", "r");
     char singleLine[1000];
     int k=0;
-    int numberOfBigWords=0;
+    int numberOfBigWords=0; ///Bliver også brugt som antal ord i strengen med store nøgleord
 
 	///ADSKILLER STRING OP I ORD OG PUTTER DEM I word4Word
 	while(ptr != NULL)	{
@@ -23,9 +27,10 @@ int main()
 		i++;
 		numberOfWords++;
 	}
-	///LÆSER bigWords.txt og
+	///LÆSER bigWords.txt og giver dem til singleLine
 while (!feof(fPointer)){
     fgets(singleLine, 150, fPointer);
+    ///tager singleLine og splitter den
     char *ptr2 = strtok(singleLine, delim);
     while (ptr2 != NULL){
 
@@ -35,29 +40,32 @@ while (!feof(fPointer)){
         numberOfBigWords++;
     }
 }
+///Printer det splittede fra bigWords.txt
 for (int q=0; q<12; q++){
     printf("%s\n", bigKeyword[q]);
 }
 	fclose(fPointer);
 
-
+///Printer strengen/titlen på artiklen
 	printf("\n");
 	for (int i=0; i<numberOfWords; i++){
         printf("%s \n", word4Word[i]);
 	}
 
-
+///tjekker om bigKeyWords mathcer med strengen/titlen på artiklen
 	for (int i=0; i<numberOfWords; i++){
         for (int j=0; j<numberOfBigWords; j++){
-
             if (strcmp(word4Word[i], bigKeyword[j])==0){
+                ///Giver fakeNewsPoints 3 point, da det er store nøgleord
                 fakeNewsPoints+=3;
             }
 
 
         }
 	}
+	///Tjekker om det er nødvendigt at tjekke de mellemstore ord
 	if (fakeNewsPoints<=3){
+            ///Tjekker om mediumKeyword matcher med artiklens titel
     for (int i=0; i<numberOfWords; i++){
        for (int j=0; j<NUMBEROFMEDIUMWORDS; j++){
             if (strcmp(word4Word[i], mediumKeyword[j])==0){
@@ -65,7 +73,9 @@ for (int q=0; q<12; q++){
             }
         }
 	}
+	///Tjekker om det er nødvendigt at tjekke de små ord
         if(fakeNewsPoints<=5){
+                ///sammenligner smallKeyword med word4Word (strengen brugt)
             for (int i=0; i<numberOfWords; i++){
                 for (int j=0; j<NUMBEROFSMALLWORDS; j++){
                     if (strcmp(word4Word[i], smallKeyword[j])==0){
@@ -75,7 +85,7 @@ for (int q=0; q<12; q++){
             }
         }
 	}
-
+///Printer hvor mange point der er
 	printf("%d", fakeNewsPoints);
 	return 0;
 }
